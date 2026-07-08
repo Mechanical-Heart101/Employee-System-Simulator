affdf2ac847912c30de48273179f711b4574b197
MegaByteMe <emmanueltemiloluwa777@gmail.com>
2026-07-08 14:46:46 +0000

    completed the payroll and payrollApp classes, standby for updates

---
+++ Payroll.java
@@ -0,0 +1,103 @@
+import java.util.ArrayList;
+
+public class Payroll
+{
+    private ArrayList<Employee> employees;
+
+    /**
+     * This is the constructor for the payroll class
+     */
+    public Payroll()
+    {
+        employees = new ArrayList<>();
+    }
+
+    /**
+     * This method adds Employee objects into the list
+     * @param String name
+     * @param int baseSalary
+     */
+    public void addFullTimeEmployee(String name, int baseSalary)
+    {
+        Employee newEmployee = new FullTimeEmployee(name, baseSalary);
+        employees.add(newEmployee);
+    }
+
+    /**
+     * This mehtod is meant to make a part time employee.
+     * @param String name
+     * @param Int baseSalary
+     * @param Int hours worked
+     */
+    public void addPartTimeEmployee(String name, int baseSalary, int hoursWorked)
+    {
+        Employee partTimeEmployee = new PartTimeEmployee(name, baseSalary, hoursWorked);
+        employees.add(partTimeEmployee);
+    }
+
+    /**
+     * This is the remove employee method
+     * @param int employee Id 
+     */
+    public void removeEmployee(int Id)
+    {
+        Employee searchObject = findEmployeeById(Id);
+        employees.remove(searchObject);
+    }
+
+    /**
+     * This is a helper method to find ID. Will probably be important later.
+     * @param Int Id of the employee
+     * @return boolean result of search
+     */
+    public boolean findById(Employee newEmployee)
+    {
+        boolean found = false;
+
+        for (Employee employee : employees)
+        {
+            if (employee.getId() == newEmployee.getId())
+            {
+                found = true;
+            }
+        }
+        return found;
+    }
+
+    /**
+     * This is a helper method to return the object with the associated ID
+     * @param Int ID
+     * @return Employee object associated with ID
+     */
+    public Employee findEmployeeById(int Id)
+    {
+        Employee foundEmployee = null;
+
+        for (Employee employee : employees)
+        {
+            if (employee.getId() == Id)
+            {
+                foundEmployee = employee;
+            }
+        }
+
+        if (foundEmployee == null)
+        {
+            System.out.println("Employee not found.");
+        }
+        return foundEmployee;
+    }
+
+    /**
+     * This method displays the employees
+     */
+    public void displayEmployees()
+    {
+        for (Employee employee : employees)
+        {
+            System.out.println(employee);
+        }
+    }
+
+
+}
---
+++ PayrollApp.java
@@ -0,0 +1,101 @@
+import java.util.Scanner;
+
+public class PayrollApp 
+{
+    public static void main (String[] args)
+    {
+        Payroll payroll = new Payroll(); // new payroll object
+        Scanner scanner = new Scanner(System.in); // scanner object
+
+        boolean running = true;
+
+        while (running)
+        {
+            System.out.println("=== Payroll ===");
+            System.out.println("1. Add full time employee.");
+            System.out.println("2. Add part time employee.");
+            System.out.println("3. View employees");
+            System.out.println("4. Find Employee");
+            System.out.println("5. Remove employee");
+            System.out.println("6. Exit");
+
+            int choice = scanner.nextInt();
+            scanner.nextLine(); // consume leftover new Line
+
+            switch (choice)
+            {
+                case 1 : // adding a new full time employee
+                    System.out.println("Please input the name: ");
+                    String name = scanner.nextLine();
+
+                    System.out.println("Please input the base salary: ");
+                    int baseSalary = scanner.nextInt();
+                    scanner.nextLine();
+
+                    payroll.addFullTimeEmployee(name, baseSalary);
+                    System.out.println("Employee successfully added!");
+
+                    break;
+
+                case 2 : // adding a new part Time employee
+                    System.out.println("Please input the name: ");
+                    name = scanner.nextLine();
+
+                    System.out.println("Please input the base salary");
+                    baseSalary = scanner.nextInt();
+                    scanner.nextLine();
+
+                    System.out.println("Please input the hours worked: ");
+                    int hoursWorked = scanner.nextInt();
+                    scanner.nextLine();
+
+                    payroll.addPartTimeEmployee(name, baseSalary, hoursWorked);
+
+                    break;
+
+                case 3 : // viewing employees
+                    payroll.displayEmployees();
+                    
+                    break;
+
+                case 4 : // find employee by id
+                    System.out.println("Please input employee id");
+                    int employeeId = scanner.nextInt();
+                    scanner.nextLine();
+
+                    Employee found = payroll.findEmployeeById(employeeId);
+
+                    if (found == null)
+                    {
+                        System.out.println("There is no such employee.");
+                    }
+                    else
+                    {
+                        System.out.println(found);
+                    }
+                    
+                    break;
+
+                case 5 : // remove employee
+                    System.out.println("Please input the employee Id");
+                    employeeId = scanner.nextInt();
+                    scanner.nextLine();
+                    
+                    payroll.removeEmployee(employeeId);
+
+                    break;
+                
+                case 6 : // exiting
+                    running = false;
+                    break;
+                
+                default :
+                    System.out.println("Invalid input! Please select correctly");
+
+            }
+        }
+
+        scanner.close(); // close the scanner to prevent memory leaks.
+    }
+    
+}
